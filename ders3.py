829da28047a5a9d4e81c51b923d87f5164ab93bc
sametatabasch <sametatabasch@gmail.com>
2019-12-27 13:30:45 +0300

    Yorumlar eklendi

--- ders3.py
+++ ders3.py
@@ -2,10 +2,10 @@
 Kullanıcının girdiği sayının tek mi çift mi olduğunu belirleyen program
 '''
 
+sayi = input("Sayı Girin:") # input değişkeni string değer döner
+sayi =int(sayi) # string olan sayi değişkeni integer yapılıyor
 
-sayi = int(input("Sayı Girin:"))
-
-if sayi % 2 == 0:
-    print("Sayı Çifttir")
+if sayi % 2 == 0: # Eğer sayının 2 ile bölümünden kalan 0 ise
+    print("Sayı Çifttir") # Yaz Sayı Çifttir
 else:
-    print("Sayı Tektir")
+    print("Sayı Tektir") # Yaz Sayı Çifttir
--- ders4.py
+++ ders4.py
@@ -1,25 +1,32 @@
 '''
-Kullanıcıdan cinsiyetini ve yaşını aldıktan sonra eğer yaşı 20 den büyük ve cinsiyeti erkek ise "askere gitmelisin "
-değilse "şimdilik askere gitmene gerek yok" mesasını yazdıran programın akış diyagramı ve algoritmasını yazın.
+Aşağıdaki kurallara göre Askere gitme durumunu belirleyen program
+k1- Cinsiyet kadın ise "Askere gidemezsin" mesajı yazılacak
+k2- Cinsiyet erkek ise kullanıcıdan yaşı istenecek
+k3- yaş 20 ile 35 arasında ise "Askere Gitmelisin" mesajı yazılacak
+k4- yaş 35 den büyük ise "Askere gitmek için çok geç" mesajı yazılacak
+k5-  yaş 20 den küçük ise "Askere gitmek için biraz daha beklemelisin" mesajı yazılacak
 
 1 Başla
-2 yas, cins
-3 Sayı Al "Yaşınız: ", yas
-4 Al "Cinsiyet (erkek/kadın):", cins
-5 Eğer yas>20 and cins=="erkek" ise Yaz "Askere Gitmelisin"
-6 değilse Yaz "Şimdilik askere gitmene gerek yok"
-7 Bitir
+2 Al "Cinsiyet (erkek/kadın):", cins
+3 cins = cins.strip()
+4 Eğer cins == "kadın" or cins == "Kadın" or cins == "K" ise Yaz "Askere gidemezsin"
+5 Eğer değilse cins == "erkek" or cins == "Erkek" or cins == "E" ise
+    6 Sayı Al "Yaşınızı Giriniz:", yas
+    7 Eğer yas >= 20 and yas <= 35 ise Yaz "Askere Gitmelisin"
+    8 Eğer Değilse yas > 35 ise Yaz "Askere gitmek için çok geç"
+    9 Değilse Yaz "Askere gitmek için biraz daha beklemelisin"
+10 Bitir
 '''
 
 cins = input("Cinsiyet (erkek/kadın):")
 cins = cins.strip()
 if cins == "kadın" or cins == "Kadın" or cins == "K":
     print("Askere gidemezsin")
-elif cins == "erkek" or cins == "Erkek"or cins == "E":
+elif cins == "erkek" or cins == "Erkek" or cins == "E":
     yas = int(input("Yaşınızı Giriniz:"))
-    if yas >= 20 and yas<=35:
+    if yas >= 20 and yas <= 35:
         print("Askere Gitmelisin")
-    elif yas>35:
+    elif yas > 35:
         print("Askere gitmek için çok geç")
     else:
         print("Askere gitmek için biraz daha beklemelisin")
