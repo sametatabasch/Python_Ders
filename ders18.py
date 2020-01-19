aea7e25acd2f7c981e48dd4ae7c0413b39811b7f
sametatabasch <sametatabasch@gmail.com>
2020-01-19 20:38:52 +0300

    fonksiyonda düzenleme yapıldı

--- ders18.py
+++ ders18.py
@@ -1,13 +1,11 @@
 def asalMi(s):
     asal = True
-
+    if s < 2: return False
     for i in range(2, s, 1):
         if s % i == 0:
             asal = False
             break
     return asal
-
 for i in range(1, 51):
     if asalMi(i):
         print(i)
-
